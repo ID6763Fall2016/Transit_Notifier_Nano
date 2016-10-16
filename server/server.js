b7e28d36a03a45543f7721e1244f9fa925ce22fb
Lei <xulei@gatech.edu>
2016-10-16 23:55:05 +0000

    [+] Success in faking browser to trick nextbus new API.

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@
 var express    = require('express')        // call express
 var app        = express()                 // define our app using express
 app.use('/m', express.static("static"))
-var http_server = require("http").createServer(app)
+var http = require("http")
+var http_server = http.createServer(app)
 
 var bodyParser = require('body-parser');
 // configure app to use bodyParser()
@@ -59,9 +60,55 @@ router.get('/', function(req, res) {
 })
 
 router.get('/suggest', function(req, res) {
-    res.json({
-      "clough": {"next": [4, 6, 10], "ahead": 8}
-    , "klaus": {"next": [2, 8], "ahead": 6}
-    });
+    res.json(answer)
 })
+var answer = {
+      "clough": {"next": [-1], "ahead": -1, "crowded": true}
+    , "klaus": {"next": [-1], "ahead": -1, "crowded": false}
+}
+
+var user_agent = "Mozilla/5.0"
+var home_req_opts = {
+    "host": "www.nextbus.com"
+    , "path": "/"
+    , "headers": {
+          "user-agent": user_agent
+    }
+}
+var api_headers = {
+    "referer": "http://www.nextbus.com"
+    , "cookie": ""
+    , "user-agent": user_agent
+}
+var ask_interval_id = null
+function ask_next_bus() {
+    http.get({
+        "host": "www.nextbus.com"
+        , "path": "/api/pub/v1/agencies/atlanta-sc/routes/ASC/stops/4560589/predictions?coincident=true&direction=ASC_0_var1&key=f87420183e937d06913347ded6d8777a&timestamp=" + new Date().getTime()
+        , "headers": api_headers
+    }, function(res) {
+        res.on("data", function(chunk) {
+            console.log("API Body: " + chunk)
+        })
+    }).on("error", function(err) {
+        console.log("API query error, %s, restart session in 5 seconds...", error.message)
+        setTimeout(restart_session, 5000)
+    })
+}
+
+function restart_session() {
+    http.get(home_req_opts, function(res) {
+        if('set-cookie' in res.headers) {
+            api_headers["cookie"] = res.headers["set-cookie"]
+            console.log("Cookie recorded:D")
+        }
+        res.resume()
+        ask_interval_id = setInterval(ask_next_bus, 5000)
+    }).on("error", function(e) {
+        console.log("Got error, %s, retry in 5 seconds...", error.message)
+        setTimeout(restart_session, 5000)
+    })
+}
+
+restart_session()
 
