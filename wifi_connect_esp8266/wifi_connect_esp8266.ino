f47b5b7768d101a3fd774e10f534ef607ecb781a
Lei Xu <lei.xu.thu02@gmail.com>
2016-10-18 20:45:02 -0400

    [+] Light up different sections.

--- wifi_connect_esp8266/wifi_connect_esp8266.ino
+++ wifi_connect_esp8266/wifi_connect_esp8266.ino
@@ -5,6 +5,28 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
+#include <Adafruit_DotStar.h>
+#include <SPI.h>
+
+#define NUMPIXELS 74
+#define DATAPIN    4
+#define CLOCKPIN   5
+
+#define CLOUGH 1
+#define KLAUS 0
+
+Adafruit_DotStar strip = Adafruit_DotStar(
+  NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BGR);
+int8_t ranges[][3][2] = {
+  /*klaus*/ { /*trolley*/{11, 16}, /*exp*/{0, 5}, /*walk*/{6, 10}}, 
+  /*clough*/{ /*exp*/{32, 37}, /*trolley*/{21, 25}, /*walk*/{27, 31}}
+};
+
+uint32_t colors[][4] = {
+  {0xc18b18, 0xfcff16, 0x11F303, 0x1229E9},
+  {0xfcff16, 0xc18b18, 0x11F303, 0x1229E9}
+};
+
 // GT config
 const char* ssid     = "GTother";
 const char* password = "GeorgeP@1927";
@@ -41,6 +63,12 @@ void setup() {
   
   http.setReuse(true);
   http.collectHeaders(header_keys, header_cnt);
+  
+  strip.begin();
+  for(int i = 0; i < NUMPIXELS; i++) {
+    strip.setPixelColor(i, 0);
+  }
+  strip.show();
 }
 
 void loop() {
@@ -60,6 +88,8 @@ void loop() {
     int c = suggest(root, "clough", "klaus");
     int k = suggest(root, "klaus", "clough");
     Serial.printf("Suggestion: clough = %d, klaus = %d\n", c, k);
+    light_up(KLAUS, k, false);
+    light_up(CLOUGH, c, false);
   } else {
     iwconfig();
     // TODO use led or dot start to indicate network failure. 
@@ -83,6 +113,30 @@ uint8_t suggest(JsonObject &root, const char *major, const char *alt) {
   double n2 = root[major]["walk"];
   
   return n0 < n1? (n0 < n2? 0 : 2) : (n1 < n2? 1 : 2);
+}
+
+int8_t prior[] = {-1, -1};
+void light_up(int8_t id, int8_t suggestion, bool pulse) {
+  int8_t s0 = prior[id];
+  Serial.printf("p0, id = %d, suggestion = %d\n", id, suggestion);
+  Serial.println("p1");
+  if(suggestion != s0) {
+    Serial.println("p2");
+    if(0 <= s0 && s0 < 3) {
+      for(int i = ranges[id][s0][0]; i < ranges[id][s0][1]; i++) {
+        strip.setPixelColor(i, 0);
+      }
+    }
+    Serial.printf("p3 %d to %d\n", ranges[id][suggestion][0], ranges[id][suggestion][1]);
+    for(int i = ranges[id][suggestion][0]; i < ranges[id][suggestion][1]; i++) {
+      uint32_t c = colors[id][suggestion];
+      Serial.printf("Setting pixel %d to #%X\n", i, c);
+      strip.setPixelColor(i, c);
+    }
+    strip.show();
+    prior[id] = suggestion;
+  }
+  // pulsing loop
 }
 
 void iwconfig() {
